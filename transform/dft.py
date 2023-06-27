d0c948fb9fe8f25ce8576a69431f0983a27c04a5
sexozavr <daniildiveev@mail.ru>
2023-06-27 23:01:06 +0300

    [ADD] add transform package

---
+++ transform/dft.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+
+class FourierTransform:
+    def __init__(self, signal: np.ndarray):
+        self.signal = signal
+
+    def transform(self) -> np.ndarray:
+        pass
+
+
